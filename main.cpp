ffd7c786ddc5dee89ed2944eed0bea776c0579e0
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-10-10 22:17:44 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "utils/utils.cpp" // Import the file utils functions
+
+using namespace std;
+
+int main() {
+  cout << "Dorime" << endl;
+  
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# This is my custom library for C++
+
+I use this library for the projects and learning with C++ in my university
+
+The folder utils contains the magic.
